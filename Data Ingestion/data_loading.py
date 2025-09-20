3a2241675280cf30393532e1eae586029401789e
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2025-09-20 17:55:47 +0200

    [fix] add metadata for wikipedia articles

--- Data Ingestion/data_loading.py
+++ Data Ingestion/data_loading.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import pandas as pd
+
+
 from llama_index.core import SimpleDirectoryReader, Document
 from llama_index.core import download_loader
 
@@ -7,15 +11,26 @@ def read_local_documents(data_dir: str) -> list[Document]:
         input_dir=data_dir,
     ).load_data()
 
-def read_wikipedia_article() -> list[Document]:
+
+def read_wikipedia_article(wiki_path: str) -> list[Document]:
     WikipediaReader = download_loader("WikipediaReader")
     loader = WikipediaReader()
-    docs = loader.load_data(pages=['Megaraptora'])
-    return docs
+    article_data = pd.read_csv(wiki_path, sep=';')
+    raw_articles = loader.load_data(pages=article_data['name'])
+    processed_articles = []
+    for idx, article in enumerate(raw_articles):
+        existing_meta = article.metadata
+        article.metadata = {
+            **existing_meta,
+            **article_data.iloc[[idx]].to_dict('records')[0],
+            "ingestion_date": str(datetime.today().date()),
+        }
+        processed_articles.append(article)
+    return processed_articles
 
 
-def load_data(data_dir: str) -> None:
+def load_data(data_dir: str, wiki_path: str) -> None:
     documents = read_local_documents(data_dir)
-    documents.extend(read_wikipedia_article())
+    documents.extend(read_wikipedia_article(wiki_path))
     return documents
 
--- Data Ingestion/main.py
+++ Data Ingestion/main.py
@@ -3,11 +3,12 @@ import argparse
 from data_loading import load_data
 
 def main(args):
-    documents = load_data(args.data_dir)
+    documents = load_data(args.docs_dir, args.wiki_path)
     print(f"Loaded {len(documents)} documents.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data-dir', '-d', required=True, help='A name of environment variable with data directory')
+    parser.add_argument('--docs-dir', '-d', required=True, help='A local data directory')
+    parser.add_argument('--wiki-path', '-w', required=True, help = 'A .csv file with metadata for the Wikipedia articles')
     args = parser.parse_args()
     main(args)
