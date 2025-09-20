47a8f1e75689984b88dac38bc267286c6e5d8bed
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2025-09-20 23:26:04 +0200

    [feat] add data chunking

---
+++ Data Ingestion/data_chunking.py
@@ -0,0 +1,25 @@
+import logging
+
+from llama_index.core.node_parser import SemanticSplitterNodeParser
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Document
+from llama_index.core.schema import BaseNode
+
+logger = logging.getLogger('data_loading')
+
+def chunk_data(documents: list[Document], model: str) -> list[BaseNode]:
+
+    embed_model = HuggingFaceEmbedding(model_name=model)
+
+    node_parser = SemanticSplitterNodeParser(
+        buffer_size=1, 
+        breakpoint_percentile_threshold=95,
+        embed_model=embed_model
+    )
+    
+    return node_parser.get_nodes_from_documents(documents)
+
+
+
+
+
--- Data Ingestion/main.py
+++ Data Ingestion/main.py
@@ -1,14 +1,25 @@
 import argparse
+import logging
 
 from data_loading import load_data
+from data_chunking import chunk_data
+
+logger = logging.getLogger('data_loading')
+logger.setLevel(logging.INFO)
+
 
 def main(args):
     documents = load_data(args.docs_dir, args.wiki_path)
-    print(f"Loaded {len(documents)} documents.")
+    logger.info("Loaded %s documents.", len(documents))
+    nodes = chunk_data(documents, args.hugging_face_model)
+    logger.info("Split %s documents into %s nodes.", len(documents), len(nodes))
+    print(f"First node text: {nodes[0].text[:200]}...")
+    print(f"First node metadata: {nodes[0].metadata}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--docs-dir', '-d', required=True, help='A local data directory')
     parser.add_argument('--wiki-path', '-w', required=True, help = 'A .csv file with metadata for the Wikipedia articles')
+    parser.add_argument('--hugging-face-model', '-hfm', help = 'An embedding model for data chunking from HuggingFace', default="BAAI/bge-small-en-v1.5")
     args = parser.parse_args()
     main(args)
